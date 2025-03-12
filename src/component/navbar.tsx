83ef2f2184d7d46c3e9f83118f6ebe1a05a042f2
Atika72 <atika72@gmail.com>
2025-03-13 01:49:42 +0600

    add new logo and update styles

--- src/component/navbar.tsx
+++ src/component/navbar.tsx
@@ -5,21 +5,22 @@ import MobileNav from "./mobileNav";
 export default function Navbar() {
   return (
     <header className="text-zinc-800 p-4 flex justify-between items-center">
-      <div className="flex flex-row space-x-3 items-center px-4">
+      <div className="flex flex-row space-x-2 items-center px-4">
         <Image
-          src="/images/fork.png"
+        className="items-center justify-center"
+          src="/images/charity-1.gif"
           alt="Tasty Tales"
-          width={30}
-          height={30}
+          width={40}
+          height={40}
         />
-        <h1 className="logo-text text-3xl font-bold">Tasty Tales</h1>
+        <h1 className="logo-text text-3xl font-extrabold pt-3">Tasty Tales</h1>
       </div>
-      <div className="hidden lg:flex flex-row space-x-5 px-5 justyify-center text-lg md:px-7 ">
+      <div className="hidden lg:flex flex-row space-x-5 px-5 justyify-center text-xl md:px-7 ">
         <Link href={"/"}>Home</Link>
         <Link href={"/about"}>About</Link>
         <Link href={"/contact"}>Contact</Link>
       </div>
-      <div className="hidden lg:flex space-x-5 px-5 text-lg">
+      <div className="hidden lg:flex space-x-5 px-5 text-xl font-normal">
         <Link href={"/sign-in"}>Sign In</Link>
         <Link href={"/sign-up"}>Sign Up</Link>
       </div>
