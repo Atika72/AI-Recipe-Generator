83ee6854b65ee787047ed5b2a51fb6f051412718
Atika72 <atika72rahman@gmail.com>
2025-03-19 00:06:59 +0600

    Add ExploreRecipe and PricingSection components; remove unused side-dish and category components

--- src/component/sideDIshList.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function SideDIshList() {
-  return (
-    <div>
-      <ol>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, natus molestias numquam placeat dignissimos sequi quis facilis laborum accusantium, minima nemo! Animi eveniet saepe, labore qui totam expedita? Praesentium, adipisci.</ol>
-    </div>
-  )
-}
